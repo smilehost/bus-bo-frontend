acbf72471c280c79d7a2de40025caba6ae96fc47
PanyawutS <panyawut.s@kkumail.com>
2025-05-13 15:56:20 +0700

    Modify: Fix Hide Button Bug

--- app/components/Table/CompanyTable.tsx
+++ app/components/Table/CompanyTable.tsx
@@ -121,6 +121,7 @@ export default function CompanyTable({
                             <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                           </svg>
                         </button>
+                        {/* ซ่อนปุ่ม Delete */}
                         <button
                           onClick={() => onDelete(company.id)}
                           className="hidden p-1.5 bg-red-50 rounded-lg text-red-600 hover:bg-red-100 transition-colors hover:shadow-sm"
