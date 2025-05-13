0b5cdb04f592d70bdcac02eafcf5d00cae1137eb
PanyawutS <panyawut.s@kkumail.com>
2025-05-13 16:12:38 +0700

    Modify: Edit ChangeStatus Icon

--- app/components/Table/MemberTable.tsx
+++ app/components/Table/MemberTable.tsx
@@ -23,7 +23,7 @@ const ActionButton: React.FC<ActionButtonProps> = ({ type, onClick }) => {
           viewBox="0 0 20 20"
           fill="currentColor"
         >
-          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+          <path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" />
         </svg>
       ),
     },
