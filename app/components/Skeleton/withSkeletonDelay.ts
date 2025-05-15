e41d56eac87ddf92bb232dd3abde7398ba7e663c
OverBig <kobe1234.aaa@gmail.com>
2025-05-15 14:16:29 +0700

    delay800

--- app/components/Skeleton/withSkeletonDelay.ts
+++ app/components/Skeleton/withSkeletonDelay.ts
@@ -1,4 +1,4 @@
-export function withSkeletonDelay(setSkeleton: (val: boolean) => void, delay = 350) {
+export function withSkeletonDelay(setSkeleton: (val: boolean) => void, delay = 800) {
     const timeoutId = setTimeout(() => {
       setSkeleton(true);
     }, delay);
