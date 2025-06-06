4cfa2772fccb5df18f4c450755e973538376baf2
OverBig <kobe1234.aaa@gmail.com>
2025-06-06 17:03:56 +0700

    ควย

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,20 @@
+import { log } from 'console';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
+  console.log("===========================================",url.pathname);
+  
 
   if (url.pathname === '/bu/manage-admin') {
     url.pathname = '/bu/manage-members'; // rewrite ไป path จริง
+    console.log("===========================================2",url.pathname);
     return NextResponse.rewrite(url);
   }
 
   if (url.pathname === '/bu/manage-employee') {
     url.pathname = '/bu/manage-members'; // ใช้หน้าเดียวกัน
+    console.log("===========================================3",url.pathname);
     return NextResponse.rewrite(url);
   }
 
