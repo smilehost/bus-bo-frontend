9bd27e2e82284caa8b24068f39258b2712157581
Phitakpong.s <pportler.sss@gmail.com>
2025-06-13 13:32:20 +0700

    Update: type more

--- utils/debounce.ts
+++ utils/debounce.ts
@@ -1,9 +1,11 @@
-export function debounce<F extends (...args: unknown[]) => void>(func: F, delay: number) {
-  let timer: ReturnType<typeof setTimeout>;
-  return (...args: Parameters<F>) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-}
+// src/utils/debounce.ts
+export function debounce<F extends (...args: any[]) => void>(func: F, delay: number) {
+    let timer: ReturnType<typeof setTimeout>;
+    return (...args: Parameters<F>) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  }
+  
